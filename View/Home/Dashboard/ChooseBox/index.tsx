4195021c0d7d74abb0cb8a76b33962e8f84d4b75
Shankhapriya <shankhapriya2015@gmail.com>
2023-08-05 15:52:57 +0530

    CooseBox

--- View/Home/Dashboard/ChooseBox/index.tsx
+++ View/Home/Dashboard/ChooseBox/index.tsx
@@ -9,7 +9,7 @@ const ChooseBox = () => {
          <TouchableOpacity style={{backgroundColor: '#e1e8e3',height:55,width:'95%', margin:10,borderRadius:10,}}>
          <Text style={{fontSize:15, fontWeight:'500',margin:15,marginLeft:20}}>Small Box</Text>
          </TouchableOpacity>
-          <View style={{height:300,width:200}}>
+          <View >
            <Image
              source={{uri: 'https://cdn2.iconfinder.com/data/icons/shopping-378/100/shopping-cart-full-shopping-carts-goods-bag-box-product-512.png'}}
              style={{width: 200, height: 200, resizeMode: 'contain'}}
@@ -20,7 +20,7 @@ const ChooseBox = () => {
          <TouchableOpacity style={{backgroundColor: '#e1e8e3',height:55,width:'95%', margin:10,borderRadius:10,}}>
          <Text style={{fontSize:15, fontWeight:'500',margin:15,marginLeft:20}}>Medium Box</Text>
          </TouchableOpacity>
-          <View style={{height:300,width:200}}>
+          <View >
            <Image
              source={{uri: 'https://cdn2.iconfinder.com/data/icons/shopping-378/100/shopping-cart-full-shopping-carts-goods-bag-box-product-512.png'}}
              style={{width: 200, height: 200, resizeMode: 'contain'}}
@@ -31,7 +31,7 @@ const ChooseBox = () => {
          <TouchableOpacity style={{backgroundColor: '#e1e8e3',height:55,width:'95%', margin:10,borderRadius:10,}}>
          <Text style={{fontSize:15, fontWeight:'500',margin:15,marginLeft:20}}>Large Box</Text>
          </TouchableOpacity>
-          <View style={{height:300,width:200}}>
+          <View >
            <Image
              source={{uri: 'https://cdn2.iconfinder.com/data/icons/shopping-378/100/shopping-cart-full-shopping-carts-goods-bag-box-product-512.png'}}
              style={{width: 200, height: 200, resizeMode: 'contain'}}
