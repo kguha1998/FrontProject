4e7e47005a601b4a64154eb0321316f0296acad3
Krishnendu <krishnendu.apk013@gmail.com>
2023-08-29 13:19:05 +0530

    cart update

--- Environment.ts
+++ Environment.ts
@@ -1,18 +1,9 @@
 // export const BaseUrl = 'http://192.168.29.94:3000/api/v1';
-<<<<<<< HEAD
 // export const BaseUrl = 'http://192.168.1.7:3000/api/v1';
 export const BaseUrl = 'http://192.168.1.7:3000/api/v1';
 export const environment = {
   url: {
     loginUrl: '/customer/login',
     productUrl: '/products',
-=======
-export const BaseUrl = 'http://192.168.1.40:3000/api/v1';
-export const environment = {
-  url: {
-    loginUrl: '/customer/login',
-    addressAddUrl: '/addresses',
-    addressListUrl: '/addresses/cust',
->>>>>>> 1e67ed2219bf5dd20e9ce2bcd2b90a25bd5c90c2
   },
 };
