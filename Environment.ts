5b879f70887f098b22b70551f67d89b903dad0a5
Shankhapriya <shankhapriya2015@gmail.com>
2023-09-04 18:18:18 +0530

    commit

--- Environment.ts
+++ Environment.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-export const BaseUrl = 'http://192.168.1.40:3000/api/v1';
-// export const BaseUrl = 'http://192.168.0.105:3000/api/v1';
-=======
  export const BaseUrl = 'http://192.168.1.40:3000/api/v1';
 //export const BaseUrl = 'http://192.168.58.157:3000/api/v1';
->>>>>>> a7b6d2b1f607043fd3d5d5c9e64406cc4b9213c1
 
 export const environment = {
   url: {
