e73a0af5cfc7f63514f916e33e87979dd0f92e5f
Sujata De <sujatade1999@gmail.com>
2023-09-07 16:54:57 +0530

    orderDetatails

--- View/Home/Order/OrderDetail/index.tsx
+++ View/Home/Order/OrderDetail/index.tsx
@@ -1,15 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import OrderDetailView from './OrderDetailView'
 
 const OrderDetail = ({navigation,route}:OrderDetailProps) => {
+  const [currentStep, setCurrentStep] = useState(0);
+
+  useEffect(() => {
+    // Simulate the progress of order status
+    const interval = setInterval(() => {
+      if (currentStep < 3) {
+        setCurrentStep((prevStep) => prevStep + 1);
+      } else {
+        clearInterval(interval);
+      }
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [currentStep]);
+
   return (
-    <View>
+    <View >
+      
       <OrderDetailView navigation={navigation}/>
+     
     </View>
+
+
+
   )
 }
 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
 export default OrderDetail
 interface OrderDetailProps{
     navigation?: any;
