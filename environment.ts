cdc51da7dd35f19a2943b047d7462bb5567df261
Arijit Kow <arijitkows@gmail.com>
2023-09-12 13:39:10 +0530

    environment changed

---
+++ environment.ts
@@ -0,0 +1,20 @@
+export const BaseUrl = 'http://192.168.1.7:3000/api/v1';
+
+export const environment = {
+  url: {
+    loginUrl: '/customer/login',
+    signupUrl: '/customer/signup',
+    addressAddUrl: '/addresses',
+    addressListUrl: '/addresses/cust',
+    productUrl: '/products',
+    commodityUrl:'/products',
+    addressFetchUrl: '/addresses',
+    addressEditUrl: '/addresses',
+    productInput: 'order/latestCalculateorderprice',
+    paymentModeUrl: '/domain',
+    addressDeleteUrl:'/addresses/delete',
+    userDetailEdit:'/customer/cust',
+    placeorderurl:'/order/CreateOrder',
+    orderlist:'/order/cust',
+  },
+};
